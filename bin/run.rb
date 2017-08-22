63bb1edb136956c3c304493d330227e8ce1ff1ef
Bobby Huang <zhenghuahuang@gmail.com>
2017-08-22 15:05:46 -0400

    refactored code in run into lib/console

--- bin/run.rb
+++ bin/run.rb
@@ -13,43 +13,47 @@ require_relative '../config/environment'
         #Ask if user wants 1)No rain or snow, 2)Mostly no rain or snow, 2)Mostly rain, 3)Mostly snow
           #=>return 3 of the locations with the cheapest flights, ask if they want to see more or if they want to exit
     #2)Cheapest flight anywhere => returns chepeast flight to any location
-print "Enter location:  "
-location = gets.chomp
-city = Location.find_by_name_or_airport(location)
 
-print "\nSort By\n"
-puts "=> 1) Price \n   2) Travel Time\n   3) Departure Time"
-print "Change sort category (enter new number or N):"
-sort = gets.chomp
+Console.new.main_menu_user_input_loop
 
-results = if sort.downcase == "n" || sort == "1"
-  city.order_flights_by
-elsif sort == "2"
-  city.order_flights_by("travel_time")
-else sort == "3"
-  city.order_flights_by("departure_time")
-end
-# binding.pry
 
-if results.length >= 3
-  for i in 0..2
-    puts "-------------------"
-    puts "| Price       #{results[i].price}|"
-    puts "| Travel Time #{results[i].travel_time} |"
-    puts "-------------------"
-    puts ""
-  end
-end
-
-if results.length > 3
-  print "See All (y/n?):  "
-  if gets.chomp == "y"
-    for i in 0..(results.length - 1)
-      puts "-------------------"
-      puts "| Price       #{results[i].price}|"
-      puts "| Travel Time #{results[i].travel_time} |"
-      puts "-------------------"
-      puts ""
-    end
-  end
-end
+# print "Enter location:  "
+# location = gets.chomp
+# city = Location.find_by_name_or_airport(location)
+#
+# print "\nSort By\n"
+# puts "=> 1) Price \n   2) Travel Time\n   3) Departure Time"
+# print "Change sort category (enter new number or N):"
+# sort = gets.chomp
+#
+# results = if sort.downcase == "n" || sort == "1"
+#   city.order_flights_by
+# elsif sort == "2"
+#   city.order_flights_by("travel_time")
+# else sort == "3"
+#   city.order_flights_by("departure_time")
+# end
+# # binding.pry
+#
+# if results.length >= 3
+#   for i in 0..2
+#     puts "-------------------"
+#     puts "| Price       #{results[i].price}|"
+#     puts "| Travel Time #{results[i].travel_time} |"
+#     puts "-------------------"
+#     puts ""
+#   end
+# end
+#
+# if results.length > 3
+#   print "See All (y/n?):  "
+#   if gets.chomp == "y"
+#     for i in 0..(results.length - 1)
+#       puts "-------------------"
+#       puts "| Price       #{results[i].price}|"
+#       puts "| Travel Time #{results[i].travel_time} |"
+#       puts "-------------------"
+#       puts ""
+#     end
+#   end
+# end
---
+++ lib/console.rb
@@ -0,0 +1,96 @@
+require 'colorized_string'
+
+class Console
+
+  def print_main_menu
+  puts <<-MENU
+  1) Search flights
+  2) Cheapest flight anywhere
+  3) List Destinations
+  4) Exit
+  MENU
+  end
+
+  def print_welcome_message
+    puts ColorizedString["\u272f" * 4 + "Farewell" + "\u272f" * 4].yellow.blink
+  end
+
+  def print_all_locations
+    Location.all.each_with_index do |location, index|
+      puts "#{index + 1}. #{location.name}"
+    end
+  end
+
+  def main_menu_user_input_loop
+    menu = nil
+    while menu != "4"
+      print_welcome_message
+      print_main_menu
+      menu = gets.chomp
+      if menu == "1"
+        search_by_location
+      elsif menu == "2"
+
+      elsif menu == "3"
+        print_all_locations
+      end
+    end
+    farewell_message
+  end
+
+  def search_by_location
+    print "Enter Destination:  "
+    location = gets.chomp
+    city = Location.find_by_name_or_airport(location)
+
+    print "\nSort By\n"
+    puts "=> 1) Price \n   2) Travel Time\n   3) Departure Time"
+    print "Change sort category (enter new number or N):"
+    sort_choice = gets.chomp
+    print_search_results(sort_flight_from_user_input(city, sort_choice))
+  end
+
+  def sort_flight_from_user_input(city, sort_choice)
+    if sort_choice.downcase == "n" || sort_choice == "1"
+      city.order_flights_by
+    elsif sort_choice == "2"
+      city.order_flights_by("travel_time")
+    elsif sort_choice == "3"
+      city.order_flights_by("departure_time")
+    end
+  end
+
+  def print_search_results(results_array)
+    if results_array.length >= 3
+      for i in 0..2
+        puts "-------------------"
+        puts "| Price       #{results_array[i].price}|"
+        puts "| Travel Time #{results_array[i].travel_time} |"
+        puts "-------------------"
+        puts ""
+      end
+    end
+
+    if results_array.length > 3
+      print "See All (y/n?):  "
+      if gets.chomp == "y"
+        for i in 0..(results_array.length - 1)
+          puts "-------------------"
+          puts "| Price       #{results_array[i].price}|"
+          puts "| Travel Time #{results_array[i].travel_time} |"
+          puts "-------------------"
+          puts ""
+        end
+      end
+    end
+  end
+
+  def farewell_message
+    message = "Farewell...".split("")
+    (message.length).times do
+      print message.shift
+      sleep(0.25)
+    end
+    print "\n"
+  end
+end
