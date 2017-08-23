f4b0d33cc6bb2e94024bfe6cbe7f0652fe62eb47
Bobby Huang <zhenghuahuang@gmail.com>
2017-08-23 10:51:34 -0400

    refactor

--- lib/console.rb
+++ lib/console.rb
@@ -39,33 +39,6 @@ class Console
     farewell_message
   end
 
-  def self.search_by_location
-    print "Enter Destination:  "
-    location = gets.chomp
-    city = Location.find_by_name_or_airport(location)
-
-    print "\nSort By\n"
-    puts "=> 1) Price \n   2) Travel Time\n   3) Departure Time"
-    print "Change sort category (enter new number or N):"
-    sort_choice = gets.chomp
-    print_search_results(sort_flight_from_user_input(city, sort_choice))
-  end
-
-  def self.cheapest_flight_random_location
-    random_locale = Location.random_locale
-    print_single_result(random_locale.cheapest_flight_for_this_location)
-  end
-
-  def self.sort_flight_from_user_input(city, sort_choice)
-    if sort_choice.downcase == "n" || sort_choice == "1"
-      city.order_flights_by
-    elsif sort_choice == "2"
-      city.order_flights_by("travel_time")
-    elsif sort_choice == "3"
-      city.order_flights_by("departure_time")
-    end
-  end
-
   def self.print_single_result(result)
     puts "-------------------"
     puts "| Price       #{result.price}|"
@@ -138,24 +111,36 @@ class Console
   end
 
   def self.print_weather_display(weather_array)
-    tt_str = ""
-    tm_str = ""
-    bm_str = ""
-    bb_str = ""
+    a_str = ""
+    b_str = ""
+    c_str = ""
+    d_str = ""
     weather_array.each do |day|
-      tt_str += "|" + "\u203E" * 9 + "|"
-      tm_str += "|    #{to_unicode(day[2])}    |"
-      bm_str += "|#{temp_to_string_min(day[0])}\u00B0/#{temp_to_string_max(day[1])}|"
-      bb_str += "|_________|"
+      a_str += "|" + "\u203E" * 9 + "|"
+      b_str += "|    #{to_unicode(day[2])}    |"
+      c_str += "|#{temp_to_string_min(day[0])}\u00B0/#{temp_to_string_max(day[1])}|"
+      d_str += "|_________|"
     end
-    puts tt_str
-    puts tm_str
-    puts bm_str
-    puts bb_str
+    puts a_str
+    puts b_str
+    puts c_str
+    puts d_str
   end
-<<<<<<< HEAD
-=======
 
-
->>>>>>> origin/search-by-weather
+  def self.print_airline_cards(airline_results = [])
+    a_str = ""
+    b_str = ""
+    c_str = ""
+    d_str = ""
+    airline_results.each do |day|
+      a_str += "|" + "\u203E" * 9 + "|"
+      b_str += "|    #{to_unicode(day[2])}    |"
+      c_str += "|#{temp_to_string_min(day[0])}\u00B0/#{temp_to_string_max(day[1])}|"
+      d_str += "|_________|"
+    end
+    puts a_str
+    puts b_str
+    puts c_str
+    puts d_str
+  end
 end
--- lib/search_by_location.rb
+++ lib/search_by_location.rb
@@ -1,6 +1,29 @@
 module SearchByLocation
-  def print_airline_cards(airline_results = [])
+  def self.search_by_location
+    print "Enter Destination:  "
+    location = gets.chomp
+    city = Location.find_by_name_or_airport(location)
 
-
+    print "\nSort By\n"
+    puts "=> 1) Price \n   2) Travel Time\n   3) Departure Time"
+    print "Change sort category (enter new number or N):"
+    sort_choice = gets.chomp
+    print_search_results(sort_flight_from_user_input(city, sort_choice))
   end
+
+  def self.cheapest_flight_random_location
+    random_locale = Location.random_locale
+    print_single_result(random_locale.cheapest_flight_for_this_location)
+  end
+
+  def self.sort_flight_from_user_input(city, sort_choice)
+    if sort_choice.downcase == "n" || sort_choice == "1"
+      city.order_flights_by
+    elsif sort_choice == "2"
+      city.order_flights_by("travel_time")
+    elsif sort_choice == "3"
+      city.order_flights_by("departure_time")
+    end
+  end
+
 end
